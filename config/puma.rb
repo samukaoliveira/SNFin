617020bf07f4e661283b885b02331b81732c036d
Samuel <mucabatera@gmail.com>
2026-01-11 15:42:40 -0300

    configurando a porta de produção

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,11 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+if Rails.env.production?
+  port 7000
+else
+  port ENV.fetch("PORT") { 3000 }
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
