371ae996447ab60c7301afc73e63370d12cee087
samukaoliveira <54003398+samukaoliveira@users.noreply.github.com>
2026-01-21 17:01:33 -0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it 'é válido com factory' do
+    expect(build(:user)).to be_valid
+  end
 end
